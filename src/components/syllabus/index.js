7c4a1295be89c6f46ee010bc790223f1805edb5c
= <birchn@byui.edu>
2022-01-04 20:56:53 -0700

    adding coupon code information

--- src/components/syllabus/index.js
+++ src/components/syllabus/index.js
@@ -14,17 +14,24 @@ function Syllabus() {
       <h4>Course Description</h4>
       <p>
         Students admitted in this course should have previous experience working
-        with a JavaScript framework, e.g. WDD 430. This course will dive deep
-        into React development using the{' '}
+        with a JavaScript framework, e.g. WDD 430.
+      </p>
+      <div className='alert alert-primary'>
+        This course will dive deep into React development using the React - The
+        Complete Guide 2021 Academind Course. &nbsp;
         <a
-          href='https://pro.academind.com/p/react-the-complete-guide'
+          href='https://sso.teachable.com/secure/210848/checkout/3148159/react-the-complete-guide'
           rel='noopener noreferrer'
           target='_blank'>
-          React - The Complete Guide 2021
+          Purchase the course with the university discount
         </a>
-        &nbsp;Academind Course. In addition to going through the entire
-        Academind Course individually, students will work in multiple teams
-        throughout the semester and develop multiple React projects.
+        . You will need to sign up with your byui email and use the coupon code{' '}
+        <strong>BYU_React_1</strong>
+      </div>
+      <p>
+        In addition to going through the entire Academind Course individually,
+        students will work in multiple teams throughout the semester and develop
+        multiple React projects.
       </p>
       <h4>Course Goals</h4>
       <ul>
